ef798d00cb226bab3408f35f96b0af45c98f27d3
Sekiph82 <sekiphayit@hotmail.com>
2026-08-26 21:11:45 +0300

    chore: modernize Expo and Next dependencies

--- apps/mobile/babel.config.cjs
+++ apps/mobile/babel.config.cjs
@@ -2,6 +2,6 @@ module.exports = function babel(api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [require("babel-preset-expo/build/expo-router-plugin").expoRouterBabelPlugin]
+    plugins: [require("babel-preset-expo/build/plugins/expo-router-plugin").expoRouterBabelPlugin]
   };
 };
